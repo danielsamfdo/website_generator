2930109b7396461c2a1c939ff94b8621d4649478
Daniel Sam Pete Thiyagu <danielsamfdo@gmail.com>
2017-04-01 10:08:53 -0400

    Removing IFS from dblp pull script

--- personal_site/dblp_pull.sh
+++ personal_site/dblp_pull.sh
@@ -5,7 +5,7 @@ grep -o 'http://dblp.uni-trier.de/rec/bibtex[^"]*' dblp_page.html > dblp_urls.tx
 wget -i dblp_urls.txt -O dblp_tmp.html
 html2text dblp_tmp.html > dblp_tmp.bib
 r="0"
-while IFS='' read -r line; do
+while read -r line; do
  head=${line:0:1}
  if [ "$head" == "@" ]; then
   r="1"
